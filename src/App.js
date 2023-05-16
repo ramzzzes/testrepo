3b9d8c593a10d6494f1ca7cc227a50b2a607889c
Giorgi Ramazashvili <ramaza.giorgi@gmail.com>
2023-05-16 22:22:44 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,29 +9,10 @@ import Button from 'react-bootstrap/Button';
 
  
 function App() {
-  //  const ChemiTegi = styled.p`
-  //   font-size: 1.5em;
-  //   color: ${props => props.inputColor || "red"};
-  // `;
-
-  // const styles = {
-  //     color: "red",
-  // };
-
+ 
   return (
       <>
-        {/* <ChemiTegi inputColor="green">test</ChemiTegi>
-        <h1 style={styles}>Hello Style!</h1>
-        <div style={{
-            'color' : 'red',
-            'font-size' : '20px'  
-        }}>
-            test
-        </div> */}
-
-        <div>test</div>
-
-
+     
         
 
         <Button as="a" variant="danger">
